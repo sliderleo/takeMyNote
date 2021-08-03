524b1dffa938f47a910ed86730d7212a96bedc95
Leo Ming Wu <kyouyaleo@gmail.com>
2021-08-03 12:49:10 +0800

    Material Api Added

--- NoteMyNotes/app/src/main/java/com/example/notemynotes/MainActivity.java
+++ NoteMyNotes/app/src/main/java/com/example/notemynotes/MainActivity.java
@@ -1,14 +1,34 @@
 package com.example.notemynotes;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.android.material.snackbar.Snackbar;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Button mSnackBarBtn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mSnackBarBtn= findViewById(R.id.snackBar_btn);
+
+        mSnackBarBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar snackbar = Snackbar.make(view,"Note Added", Snackbar.LENGTH_LONG);
+                snackbar.setAction("Okay", new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+
+                    }
+                });
+                snackbar.show();
+            }
+        });
     }
 }
